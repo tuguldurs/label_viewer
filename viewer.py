3c03177cc59f76b3fe98bde4a1a9e6e2a4c73bcb
tuguldurs <tuguldur.s@gmail.com>
2022-02-20 18:56:44 -0500

    dataframe

--- viewer.py
+++ viewer.py
@@ -47,19 +47,35 @@ def get_all_indices() -> list:
 	return [name.split('.')[0].split('_')[-1] for name in gifs]
 
 
-def populate(index: str) -> None:
+def update_df(idx: int, eval_val: str, comment_val: str) -> None:
+	"""Updates dataframe with inputs."""
+	df.at[idx, 'eval'] = eval_val
+	df.at[idx, 'comment'] = comment_val
+
+
+def populate(index_frame: str) -> tuple:
 	"""Populates image data in separate columns."""
+	idx = indices.index(index_frame)
+
 	with col_raw:
 		st.subheader("Raw")
-		st.image(f'{datapath}/raw_{index}.png')
+		st.image(f'{datapath}/raw_{index_frame}.png')
+		eval_val = col_raw.radio('Eval:', eval_choices)
 
 	with col_ann:
 		st.subheader("Annotated")
-		st.image(f'{datapath}/annotated_{index}.png')
+		st.image(f'{datapath}/annotated_{index_frame}.png')
+		val = df.loc[df["index"] == index_frame, "eval"].iloc[0]
+		st.text(f'Stored Eval: {val}')
 
 	with col_gif:
 		st.subheader("GIF")
-		st.image(f'{datapath}/anim_{index}.gif')
+		st.image(f'{datapath}/anim_{index_frame}.gif')
+		comment_val = df.loc[df["index"] == index_frame, "comment"].iloc[0]
+		comment_val = col_gif.text_input('Comments: ', value = comment_val)
+		
+
+		st.button('SUBMIT', on_click=update_df, args=(idx, eval_val, comment_val))
 
 
 with st.sidebar:
@@ -70,12 +86,23 @@ with st.sidebar:
 indices = get_all_indices()
 pager = Pager(len(indices))
 
+@st.cache(allow_output_mutation=True)
+def make_df():
+	# Creates base dataframe to store inputs.
+	default_eval = ['Bad'] * len(indices)
+	default_comment = ['pizda'] * len(indices)
+	data = {'index': indices,
+		'eval': default_eval,
+		'comment': default_comment}
+	return pd.DataFrame(data)
+
+df = make_df()
+
 st.title("IVUS Label Review for Ohio-Health")
 col_raw, col_ann, col_gif = st.columns(3)
 
 if "page" not in st.session_state:
     st.session_state["page"] = 0
-
 
 with st.sidebar:
 	st.write(f'Number of sets = {len(indices)}')
@@ -89,6 +116,7 @@ with st.sidebar:
 	if index_goto != default_goto:
 		st.session_state['page'] = indices.index(index_goto)
 
+eval_choices = ['Good', 'Bad']
 
 if st.button("< Previous"):
 	pager.set_current(st.session_state['page'])
@@ -98,9 +126,10 @@ if st.button("Next >"):
 	pager.set_current(st.session_state['page'])
 	st.session_state['page'] = pager.next
 
+viewing_index = indices[st.session_state['page']]
+st.write(f'Viewing index: {viewing_index}')
+populate(viewing_index)
 
-st.write(f'Viewing index: {indices[st.session_state["page"]]}')
-populate(indices[st.session_state['page']])
 
-col_ann.radio('Eval:', ['Good', 'Bad'])
-col_gif.text_input('Comments: ')
+
+st.dataframe(df)
