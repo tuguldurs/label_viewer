b021a89e816d322fdf7801d797efdbe0379bc59c
tuguldurs <tuguldur.s@gmail.com>
2022-02-20 14:31:35 -0500

    view index

--- viewer.py
+++ viewer.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import os
 
-
+import pandas as pd
 import streamlit as st
 
 
@@ -11,29 +11,33 @@ st.set_page_config(layout="wide")
 
 
 class Pager:
-	"""Generates and stepper cycled indices."""
-    def __init__(self, count) -> None:
-        self.count = count
-        self.current = 0
+	"""Generates cycled stepper indices."""
 
-    @property
-    def next(self) -> int:
-        n = self.current + 1
-        # cycle to beginning
-        if n > self.count-1:
-            n -= self.count
-        return n
+	def __init__(self, count) -> None:
+		self.count = count
+		self.current = 0
 
-    @property
-    def prev(self) -> int:
-        n = self.current - 1
-        # cycle to end
-        if n < 0 :
-            n += self.count
-        return n
+	@property
+	def next(self) -> int:
+		"""Fetches next index."""
+		n = self.current + 1
+		# Cycles to beginning.
+		if n > self.count-1:
+			n -= self.count
+		return n
 
-    def set_current(self, current) -> None:
-    	self.current = current
+	@property
+	def prev(self) -> int:
+		"""Fetches previous index."""
+		n = self.current - 1
+		# Cycles to end.
+		if n < 0 :
+			n += self.count
+		return n
+
+	def set_current(self, current) -> None:
+		"""Sets current index."""
+		self.current = current
 
 
 def get_all_indices() -> list:
@@ -72,14 +76,16 @@ col_raw, col_ann, col_gif = st.columns(3)
 if "page" not in st.session_state:
     st.session_state["page"] = 0
 
+
 with st.sidebar:
 	st.write(f'Number of sets = {len(indices)}')
 	default_goto = '<select>'
 	options_goto = indices.copy()
 	options_goto.insert(0, default_goto)
 	index_goto = st.selectbox(
-     'Go to:',
-     tuple(options_goto))
+		'Go to:',
+		tuple(options_goto)
+		)
 	if index_goto != default_goto:
 		st.session_state['page'] = indices.index(index_goto)
 
@@ -87,14 +93,13 @@ with st.sidebar:
 if st.button("< Previous"):
 	pager.set_current(st.session_state['page'])
 	st.session_state['page'] = pager.prev
-	print(st.session_state['page'])
 
 if st.button("Next >"):
 	pager.set_current(st.session_state['page'])
 	st.session_state['page'] = pager.next
-	print(st.session_state['page'])
 
 
+st.write(f'Viewing index: {indices[st.session_state["page"]]}')
 populate(indices[st.session_state['page']])
 
 col_ann.radio('Eval:', ['Good', 'Bad'])
